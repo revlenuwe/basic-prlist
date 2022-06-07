b70392409039665a97df404af74249e9972cc502
Revlen <uverogre@gmail.com>
2022-06-07 21:32:32 +0200

    Setup db connection

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
 
 use App\Controllers\ProductsController;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Events\Dispatcher;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use League\Route\Router;
-use Psr\Http\Message\ServerRequestInterface;
 
 require_once "vendor/autoload.php";
+
+$capsule = new Manager();
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+]);
+
+
+$capsule->setEventDispatcher(new Dispatcher(new Container()));
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
+
+
 
 $request = ServerRequestFactory::fromGlobals(
     $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
