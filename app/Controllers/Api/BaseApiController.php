c16a7de9f6c2ecd9682178ea956a20fd62be22dc
Revlen <uverogre@gmail.com>
2022-06-09 14:04:25 +0200

    Add status code to response method

--- app/Controllers/Api/BaseApiController.php
+++ app/Controllers/Api/BaseApiController.php
@@ -8,12 +8,12 @@ use Laminas\Diactoros\Response;
 
 abstract class BaseApiController
 {
-    public function response(array $data)
+    public function response(array $data, int $status = 200)
     {
         $response = new Response();
 
         $response->getBody()->write(json_encode($data));
 
-        return $response->withAddedHeader('content-type', 'application/json')->withStatus(200);
+        return $response->withAddedHeader('content-type', 'application/json')->withStatus($status);
     }
 }
